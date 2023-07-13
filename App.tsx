231a9859cc1c8b787de93ac7fc9fe79c9aba2135
Jeewan <jeewan.jamnal@mindbowser.com>
2023-07-13 14:08:19 +0530

    Feat: Filter functionality added
    Added filter functionality bases on week, month and year

--- App.tsx
+++ App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useEffect, useId, useState } from 'react';
 import {
   ActivityIndicator,
@@ -14,10 +7,13 @@ import {
   Text,
   useColorScheme,
   View,
-  Dimensions
+  Dimensions,
+  Button,
+  TouchableHighlight,
+  StyleSheet
 } from 'react-native';
 import getApiData from './service';
-import { getDBConnection, createTable, updateTable, getDataFromTable } from './db-service';
+import { getDBConnection, createTable, updateTable, getDataFromTable, filterDbData } from './db-service';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -25,7 +21,7 @@ const windowHeight = Dimensions.get('window').height;
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const [tableData, setTableData] = useState([])
-  const id = useId()
+
   useEffect(()=>{
     try{
       getDBConnection()
@@ -37,17 +33,16 @@ function App(): JSX.Element {
     async function getData(){
       let fetchData = await getApiData()
       if(fetchData && fetchData.length>0){
-        createTable().then((data) => {
-          updateTable(fetchData).then(async(data) => {
-            console.log("updated successfully");
-            let tabelData = await getDataFromTable()
-              if(tabelData && tabelData.length>0){
-                setTableData(tabelData)
-              }
-          })
-          .catch((error) => {
-            console.log("update",error);
-          });
+        createTable().then(async(data) => {
+            updateTable(fetchData).then(async(data) => {
+              let tabelData = await getDataFromTable()
+                if(tabelData && tabelData.length>0){
+                  setTableData(tabelData)
+                }
+            })
+            .catch((error) => {
+              console.log("update",error);
+            });
         })
         .catch((error) => {
           console.log("create",error);
@@ -58,33 +53,70 @@ function App(): JSX.Element {
     
   },[])
 
-  useEffect(()=>{
-    async function getTableData() {
-      
+  const filterData=async(type:string)=>{
+    let _filteredData = await filterDbData(type)
+    console.log("length>>>>",_filteredData.length)
+    if(_filteredData && _filteredData.length>0){
+      setTableData(_filteredData)
     }
-    getTableData()
-    
-  },[])
+  }
 
   return (
     <SafeAreaView>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
       />
-      <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
-        {
-          tableData && tableData.length>0 ? tableData.map((data,index)=>{
-            return(<View key={data?.id+index} style={{width:300,height:100,backgroundColor:'#CCCCCC',borderRadius:5,alignSelf:'center',alignItems:'center',justifyContent:'center',marginTop:5}}>
-                    <Text style={{color:'#111',fontSize:12}}>Id: {data?.id}</Text>
-                    <Text style={{color:'#111',fontSize:12}}>Book name: {data?.book?.name}</Text>
-                  </View>)
-          }) :  <View style={{width:windowWidth,height:windowHeight,alignItems:'center',justifyContent:'center'}}><ActivityIndicator size="large" /></View>
-        }
-      </ScrollView>
+      
+      <View style={{width:windowWidth,height:windowHeight,paddingTop:windowHeight*.02}}>
+        <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+          <Text style={{fontSize:16, fontWeight:'bold',paddingLeft:10}}>Filter</Text>
+          <View style={{height:windowHeight*.12, marginTop:windowHeight*.02}}>
+            <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false} horizontal={true}>
+              <TouchableHighlight style={styles.buttonContainer} activeOpacity={0.6} underlayColor="#CCCCCC" onPress={()=>filterData('week')}>
+                <Text style={styles.buttonLabelStyle}>Weekly</Text>
+              </TouchableHighlight>
+              <TouchableHighlight style={styles.buttonContainer} activeOpacity={0.6} underlayColor="#CCCCCC" onPress={()=>filterData('month')}>
+                <Text style={styles.buttonLabelStyle}>Monthly</Text>
+              </TouchableHighlight>
+              <TouchableHighlight style={styles.buttonContainer} activeOpacity={0.6} underlayColor="#CCCCCC" onPress={()=>filterData('year')}>
+                <Text style={styles.buttonLabelStyle}>Yearly</Text>
+              </TouchableHighlight>
+              <TouchableHighlight style={styles.buttonContainer} activeOpacity={0.6} underlayColor="#CCCCCC" onPress={()=>filterData('name')}>
+                <Text style={styles.buttonLabelStyle}>Name</Text>
+              </TouchableHighlight>
+            </ScrollView>
+          </View>
+          
+          {
+            tableData && tableData.length>0 ? tableData.map((data,index)=>{
+              return(<View key={data?.id+index} style={{width:300,height:100,backgroundColor:'#CCCCCC',borderRadius:5,alignSelf:'center',alignItems:'center',justifyContent:'center',marginTop:5}}>
+                      <Text style={{color:'#111',fontSize:12}}>Id: {data?.id}</Text>
+                      <Text style={{color:'#111',fontSize:12}}>Time: {data?.timePlaced}</Text>
+                    </View>)
+            }) :  <ActivityIndicator size="large" />
+          }
+        </ScrollView>
+      </View>
+      
       
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width:100,
+    height:60,
+    backgroundColor:'red',
+    alignItems:'center',
+    justifyContent:'center',
+    marginLeft:10,
+    borderRadius:5
+  },
+  buttonLabelStyle:{
+    color:'#fff',
+    fontSize:14
+  }
+});
 
 export default App;
--- db-service.js
+++ db-service.js
@@ -14,10 +14,15 @@ import {enablePromise, openDatabase, SQLiteDatabase} from 'react-native-sqlite-s
 
   async function createTable(){
     // create table if not exists
-    const query = `CREATE TABLE IF NOT EXISTS BooksData (id TEXT, bettor TEXT, book TEXT,
-      bettorAccount TEXT, bookRef TEXT, timePlaced TEXT, type TEXT, subtype TEXT, oddsAmerican TEXT, atRisk TEXT, toWin TEXT, status TEXT, outcome TEXT,
-      refreshResponse TEXT, incomplete TEXT, netProfit TEXT, dateClosed TEXT, typeSpecial TEXT, bets TEXT, adjusted TEXT)`;
+    // const query = `CREATE TABLE IF NOT EXISTS BooksData (id TEXT, bettor TEXT, book TEXT,
+    //   bettorAccount TEXT, bookRef TEXT, timePlaced TEXT, type TEXT, subtype TEXT, oddsAmerican TEXT, atRisk TEXT, toWin TEXT, status TEXT, outcome TEXT,
+    //   refreshResponse TEXT, incomplete TEXT, netProfit TEXT, dateClosed TEXT, typeSpecial TEXT, bets TEXT, adjusted TEXT)`;
       
+    const query = `CREATE TABLE IF NOT EXISTS BooksData (id VARCHAR, bettor VARCHAR, book VARCHAR,
+      bettorAccount VARCHAR, bookRef VARCHAR, timePlaced VARCHAR, type VARCHAR, subtype VARCHAR, oddsAmerican VARCHAR, atRisk VARCHAR, toWin VARCHAR, status VARCHAR, outcome VARCHAR,
+      refreshResponse VARCHAR, incomplete VARCHAR, netProfit VARCHAR, dateClosed VARCHAR, typeSpecial VARCHAR, bets VARCHAR, adjusted VARCHAR)`;
+      
+
       return new Promise((resolve, reject) => {
         db.transaction((tx) => {
           tx.executeSql(query,[],(_tx,result) => {
@@ -38,7 +43,7 @@ import {enablePromise, openDatabase, SQLiteDatabase} from 'react-native-sqlite-s
         dataArray.forEach((data) => {
           tx.executeSql(
             'INSERT INTO BooksData (id, bettor, book, bettorAccount, bookRef, timePlaced, type, subtype, oddsAmerican, atRisk, toWin, status, outcome, refreshResponse, incomplete, netProfit, dateClosed, typeSpecial, bets, adjusted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            [JSON.stringify(data)],
+            [data.id, data.bettor, data.book, data.bettorAccount, data.bookRef, data.timePlaced, data.type, data.subtype, data.oddsAmerican, data.atRisk, data.toWin, data.status, data.outcome, data.refreshResponse, data.incomplete, data.netProfit, data.dateClosed, data.typeSpecial, data.bets, data.adjusted],
             (_tx, results) => {
               console.log("records updated successfully")
               resolve('records updated successfully');
@@ -65,8 +70,7 @@ import {enablePromise, openDatabase, SQLiteDatabase} from 'react-native-sqlite-s
             const data = [];
             for (let i = 0; i < rows.length; i++) {
               const row = rows.item(i);
-              const deserializedData = JSON.parse(row.id);
-              data.push(deserializedData);
+              data.push(row);
             }
             resolve(data);
           },
@@ -78,5 +82,33 @@ import {enablePromise, openDatabase, SQLiteDatabase} from 'react-native-sqlite-s
     });
   }
 
-  export {getDBConnection, createTable, updateTable, getDataFromTable}
+  async function filterDbData(type){
+    return new Promise((resolve, reject) => {
+
+      const formattedWeekStart =  type === "week" ? "2022-06-01T00:00:00Z" : type === "month" ? "2022-05-01T00:00:00Z": "2021-01-01T00:00:00Z";
+      const formattedWeekEnd =  type === "week" ? "2022-06-07T00:00:00Z" : type === "month" ? "2022-05-31T00:00:00Z": "2021-12-31T00:00:00Z";
+
+      // Execute the query with the date filter
+      db.transaction(tx => {
+        tx.executeSql(
+          `SELECT * FROM BooksData where timePlaced BETWEEN ? AND ?`,
+          [formattedWeekStart,formattedWeekEnd],
+          (_tx, results) => {
+            const rows = results.rows;
+            const data = [];
+            for (let i = 0; i < rows.length; i++) {
+              const row = rows.item(i);
+              data.push(row);
+            }
+            resolve(data);
+          },
+          error => {
+            console.error(error);
+          }
+        );
+      });
+    })
+  }
+
+  export {getDBConnection, createTable, updateTable, getDataFromTable, filterDbData}
 
